9e5f40c5570e280e5c74883998321cf00c2bd6f7
Sebastian <sebastian.narloch82@gmail.com>
2020-11-23 16:44:43 +1000

    created fron-end grid

---
+++ tic-tac-toe.js
@@ -0,0 +1,9 @@
+const gameBoard = () => {
+    const board = [[],[],[]];
+    return board;
+} 
+
+const player = (name, character) => {
+    const getName = () => name;
+    const getChar = () => character;
+}
