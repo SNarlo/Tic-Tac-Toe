43aaa7c4898a7d957c6af9f0acf7cbe9197c54bd
Sebastian <sebastian.narloch82@gmail.com>
2020-11-27 16:11:45 +1000

    Figuring out the logic for the game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,43 @@
-const gameBoard = () => {
-    const board = [[[],['O'],[]],[[],['X'],[]],[[],[],[]]];
-    const getBoard = () => console.log(board);
-    return {getBoard};
-} 
+const gameBoard = (() => {
+    const board = [[[],[],[]],[[],[],[]],[[],[],[]]];
+    // const newBoard; // resets the game board 
+    return {
+        board, 
+        // newBoard,
+    };
+})();
 
-const player = (name, character) => {
-    const getName = () => name;
-    const getChar = () => character;
+/**
+ * Game logic representing a player who playes the game
+ */
+const playerOne = () => {
+    const role = () => "";
+    // const char = setChar();
+
+    return {
+        role
+    };
+};
+
+const computer = () => {
 }
 
 
 function render() {
     gameBoard.getBoard;
 }
+
+
+/**
+ * This code assigns the player a character based on the selected character
+ */
+characterButtons = document.querySelectorAll(".character-button");
+characterButtons.forEach(character => character.addEventListener('click', () => {
+    if (character.id == 'naught') {
+        playerOne.role = 'O';
+    }
+}))
+// characterButtons.addEventListener('click', assignCharacterToPlayer);
+// function assignCharacterToPlayer(e) {
+//     console.log(e);
+// }
